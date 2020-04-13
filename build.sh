85f55c6d6eb835c1fe42aa6bc5be28a8d9a1afdb
Khanh Do <khanh.doth@gmail.com>
2020-04-13 09:35:40 +0000

    update 20

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "Build My Hello container"
+
+echo "Current user is:"
+whoami
+cd /home/host/dev/my_hello
+
+# delete running container
+docker rm -f my-running-hello
+
+# delete image
+docker image rm my-hello:lates
+
+# build new image
+docker build -t my-hello:latest .
+
+# create new container based on new image test 10
+docker run -dit --name my-running-hello -p 4000:4000 my-hello
+docker exec my-hello bin/hello start
