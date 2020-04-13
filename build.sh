76cfc7cf6cd1ffbc833ed526f25b1f3abafa4b0d
Khanh Do <khanh.doth@gmail.com>
2020-04-13 10:40:04 +0000

    update 21

--- build.sh
+++ build.sh
@@ -16,4 +16,4 @@ docker build -t my-hello:latest .
 
 # create new container based on new image test 10
 docker run -dit --name my-running-hello -p 4000:4000 my-hello
-docker exec my-hello bin/hello start
+docker exec -dit my-running-hello bin/hello start
