e28f8aa0118d2755d786295570ff6878a6e52a26
khanhdoth <khanh.doth@gmail.com>
2020-04-14 14:13:19 +0000

    Sulof16

--- build.dev.sh
+++ build.dev.sh
@@ -15,5 +15,5 @@ docker image rm my-hello:dev
 docker build -t my-hello:dev . -f dev.Dockerfile
 
 # create new container based on new image test
-docker run -dit --name my-running-dev-hello -p 4000:4000 -p 4001:4001 -v "/home/host/dev/git/my_hello:/home/dev/git/my_hello" my-hello:dev
-docker exec -dit my-running-dev-hello /bin/sh /home/dev/git/my_hello/dev.compile.sh
+docker run -dit --name my-running-dev-hello -p 4000:4000 -p 4001:4001 -v /home/host/dev/git/my_hello:/home/dev/git/my_hello my-hello:dev
+docker exec -it my-running-dev-hello /bin/sh /home/dev/git/my_hello/dev.compile.sh
