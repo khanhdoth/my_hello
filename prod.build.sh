f7b5430e927b39b197d602e26d866e04425bde95
khanhdoth <khanh.doth@gmail.com>
2020-04-20 17:04:35 +0000

    u

--- prod.build.sh
+++ prod.build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Build My Hello production container"
 
-docker exec -it my-running-jenkins sh /home/host/dev/git/my_hello/prod.build.jenkins.sh
+docker exec my-running-jenkins sh /home/host/dev/git/my_hello/prod.build.jenkins.sh
